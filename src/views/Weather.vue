a68ec9a90b3b6799b5176dd8419687ce24709e07
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-05 22:55:52 +0100

    added WeatherCard component

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -3,15 +3,19 @@
     class="bg-gradient-to-t via-red-400 from-yellow-300 to-red-400 min-h-screen"
   >
     <WeatherSearch />
+    <div>
+      <WeatherCard />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import WeatherSearch from '../components/WeatherSearch.vue'
+import WeatherSearch from '@/components/weatherSearch.vue'
+import WeatherCard from '@/components/weatherCard.vue'
 
 export default defineComponent({
-  components: { WeatherSearch },
+  components: { WeatherSearch, WeatherCard },
   setup() {
     return {}
   }
