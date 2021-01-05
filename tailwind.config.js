1cffe5cd8ed8ee2e4ff0fd55bc32e81a4896227b
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-05 20:23:43 +0100

    Added tailwind config with additional settings

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif']
+      },
+      width: {
+        112: '28rem'
+      },
+      height: {
+        '30vh': '30vh',
+        '40vh': '40vh',
+        '50vh': '50vh'
+      },
+      maxWidth: {
+        '9/10': '90%'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
