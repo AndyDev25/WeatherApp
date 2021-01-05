7310055f73d489013d71e0c1234992f2f799e026
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-05 20:27:10 +0100

    main route renamed to Weather instead of Home

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Weather from '../views/Weather.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Weather',
+    component: Weather
   },
 ]
 
