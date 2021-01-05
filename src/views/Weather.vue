36b168e63771abd86184f5d11412194c4a28620c
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-05 20:25:36 +0100

    created Weather.vue views

---
+++ src/views/Weather.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="bg-gradient-to-t via-red-400 from-yellow-300 to-red-400 min-h-screen"
+  >
+    <WeatherSearch />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import WeatherSearch from '../components/WeatherSearch.vue'
+
+export default defineComponent({
+  components: { WeatherSearch },
+  setup() {
+    return {}
+  }
+})
+</script>
