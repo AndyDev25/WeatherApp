151c8adc15aaae7fa90af944d21ab638d1b44089
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-06 01:45:17 +0100

    Added card params. and variants extends for height

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,28 @@ module.exports = {
       },
       width: {
         112: '28rem',
-        130: '35rem'
+        130: '35rem',
+        cardImage: '360px'
       },
       height: {
         '30vh': '30vh',
         '40vh': '40vh',
-        '50vh': '50vh'
+        '50vh': '50vh',
+        cardImage: '480px',
+        extendedCardImage: '520px'
       },
       maxWidth: {
         '9/10': '90%'
+      },
+      transitionDuration: {
+        250: '250ms'
       }
     }
   },
   variants: {
-    extend: {}
+    extend: {
+      height: ['hover']
+    }
   },
   plugins: []
 }
