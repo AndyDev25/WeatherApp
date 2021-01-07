37e4e3493293206af2731f81aa343831377513c1
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-07 02:21:43 +0100

    Front and Back page changed to separate components

--- src/components/weatherCard.vue
+++ src/components/weatherCard.vue
@@ -9,96 +9,19 @@
       @click="toggle.toggleTransform = !toggle.toggleTransform"
       :class="{ animationCardWrapper: toggle.toggleTransform }"
     >
-      <div class="max-w-9/10 sm:max-w-full ">
-        <div class="relative cursor-pointer frontPage">
-          <img
-            src="@/assets/image/coldCard.jpg"
-            alt="coldCard"
-            class="rounded-lg w-cardImage h-cardImage object-cover"
-          />
-          <h2
-            class="absolute top-midY left-midX text-black text-center text-4xl font-light"
-          >
-            Michalovce
-          </h2>
-        </div>
-      </div>
-      <div class="max-w-9/10 sm:max-w-full">
-        <div class="relative cursor-pointer backPage">
-          <img
-            src="@/assets/image/coldCard.jpg"
-            alt="coldCard"
-            class="rounded-lg w-cardImage h-cardImage object-cover "
-          />
-          <div class="absolute inset-0 top-32 text-black">
-            <div
-              class="font-light flex flex-col items-center justify-center text-lg"
-            >
-              <h2 class="text-lg sm:text-xl">
-                SK
-              </h2>
-              <div class="flex flex-col items-start flex-wrap ">
-                <div role="clouds">
-                  <div class="flex items-center">
-                    <img
-                      src="@/assets/svgs/cloud.svg"
-                      alt="cloud"
-                      class="w-6 h-6"
-                    />
-                    <p>Status: broken clouds,</p>
-                  </div>
-                  <div class="flex items-center">
-                    <img
-                      src="@/assets/svgs/cloud.svg"
-                      alt="cloud"
-                      class="w-6 h-6"
-                    />
-                    <p>
-                      Cloudiness: 100%
-                    </p>
-                  </div>
-                </div>
-                <div role="temperature">
-                  <div class="flex items-center">
-                    <img
-                      src="@/assets/svgs/coldTemp.svg"
-                      alt="cloud"
-                      class="w-6 h-6"
-                    />
-                    <p>Temperature: 2°C</p>
-                  </div>
-                  <div class="flex items-center">
-                    <img
-                      src="@/assets/svgs/coldTemp.svg"
-                      alt="cloud"
-                      class="w-6 h-6"
-                    />
-                    <p>Sensory temp: 2°C</p>
-                  </div>
-                </div>
-                <div role="wind">
-                  <div class="flex items-center">
-                    <img
-                      src="@/assets/svgs/windSpeed.svg"
-                      alt="cloud"
-                      class="w-6 h-6"
-                    />
-                    <p>Wind speed: 3.63 m/sec.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <frontWeatherPage />
+      <backWeatherPage />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, reactive } from 'vue'
+import frontWeatherPage from './frontWeatherCard.vue'
+import backWeatherPage from './backWeatherCard.vue'
 
 export default defineComponent({
+  components: { frontWeatherPage, backWeatherPage },
   setup() {
     const toggle = reactive({
       toggleTransform: false
