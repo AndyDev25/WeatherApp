62c5f17a0a47a0c9890438e48a5d7235b645b30a
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-07 20:17:42 +0100

    Changed static values to dynamic also added props

--- src/components/backWeatherCard.vue
+++ src/components/backWeatherCard.vue
@@ -1,35 +1,37 @@
 <template>
   <BackCardInfo>
     <h2 class="text-lg sm:text-xl">
-      SK
+      {{ country }}
     </h2>
-    <div class="flex flex-col items-start flex-wrap">
+    <div class="flex flex-col items-start flex-wrap space-y-2">
       <div role="clouds">
         <div class="flex items-center">
           <img src="@/assets/svgs/cloud.svg" alt="cloud" class="w-6 h-6" />
-          <p>Status: broken clouds,</p>
+          <p>Status: {{ status }},</p>
         </div>
         <div class="flex items-center">
           <img src="@/assets/svgs/cloud.svg" alt="cloud" class="w-6 h-6" />
-          <p>
-            Cloudiness: 100%
-          </p>
+          <p>Cloudiness: {{ Cloudiness }}%</p>
         </div>
       </div>
-      <div role="temperature">
+      <div role="temperature" class="">
         <div class="flex items-center">
           <img src="@/assets/svgs/coldTemp.svg" alt="cloud" class="w-6 h-6" />
-          <p>Temperature: 2°C</p>
+          <p>Temperature: {{ convertTemperature(Temperature) }}°C</p>
         </div>
         <div class="flex items-center">
           <img src="@/assets/svgs/coldTemp.svg" alt="cloud" class="w-6 h-6" />
-          <p>Sensory temp: 2°C</p>
+          <p>Sensory temp: {{ convertSensoryTemp(SensoryTemp) }}°C</p>
         </div>
       </div>
       <div role="wind">
         <div class="flex items-center">
           <img src="@/assets/svgs/windSpeed.svg" alt="cloud" class="w-6 h-6" />
-          <p>Wind speed: 3.63 m/sec.</p>
+          <p>Wind speed: {{ windSpeed }}m/sec</p>
+        </div>
+        <div class="flex items-center">
+          <img src="@/assets/svgs/windSpeed.svg" alt="cloud" class="w-6 h-6" />
+          <p>Wind deg: {{ windDeg }}°</p>
         </div>
       </div>
     </div>
@@ -39,10 +41,47 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import BackCardInfo from './backCardInfoContainer.vue'
+import { useStore } from 'vuex'
+import { capitalize } from 'lodash-es'
+
 export default defineComponent({
   components: { BackCardInfo },
+  props: {
+    status: {
+      type: String
+    },
+    Cloudiness: {
+      type: Number
+    },
+    Temperature: {
+      type: Number
+    },
+    SensoryTemp: {
+      type: Number
+    },
+    windSpeed: {
+      type: Number
+    },
+    windDeg: {
+      type: Number
+    },
+    country: {
+      type: String
+    }
+  },
   setup() {
-    return {}
+    const store = useStore()
+
+    const convertTemperature = (Temp: number) => {
+      return Math.floor(Temp - 273.15)
+    }
+    const convertSensoryTemp = (SensoryTemp: number) => {
+      return Math.floor(SensoryTemp - 273.15)
+    }
+    return {
+      convertTemperature,
+      convertSensoryTemp
+    }
   }
 })
 </script>
