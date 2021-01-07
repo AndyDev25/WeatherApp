72a4e707f54e3e0d75ceb3c5ac2e5ad646be1267
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-07 20:19:21 +0100

    Added props to pass data to child, removed computed and calc for temp

--- src/components/weatherCard.vue
+++ src/components/weatherCard.vue
@@ -7,27 +7,60 @@
     <div
       class="animationCardContainer flex justify-center select-none"
       @click="toggle.toggleTransform = !toggle.toggleTransform"
-      :class="{ animationCardWrapper: toggle.toggleTransform }"
+      :class="{
+        animationCardWrapper: toggle.toggleTransform
+      }"
     >
-      <frontWeatherPage />
-      <backWeatherPage />
+      <frontWeatherPage :city="city" />
+      <backWeatherPage
+        :status="status"
+        :Cloudiness="Cloudiness"
+        :Temperature="Temperature"
+        :SensoryTemp="SensoryTemp"
+        :windSpeed="windSpeed"
+        :windDeg="windDeg"
+        :country="country"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, reactive } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import frontWeatherPage from './frontWeatherCard.vue'
 import backWeatherPage from './backWeatherCard.vue'
 
 export default defineComponent({
   components: { frontWeatherPage, backWeatherPage },
+  props: {
+    city: {
+      type: String
+    },
+    status: {
+      type: String
+    },
+    Cloudiness: {
+      type: Number
+    },
+    Temperature: {
+      type: Number
+    },
+    SensoryTemp: {
+      type: Number
+    },
+    windSpeed: {
+      type: Number
+    },
+    windDeg: {
+      type: Number
+    },
+    country: {
+      type: String
+    }
+  },
   setup() {
     const toggle = reactive({
       toggleTransform: false
-    })
-    const convertToCelsium = computed((temperature: number) => {
-      return temperature - 273.15
     })
 
     return {
