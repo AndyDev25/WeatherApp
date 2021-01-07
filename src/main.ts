02fa87335e6eb210b52e7dc04714711cc85838ec
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-07 15:32:37 +0100

    Removed EventBus replaced by Vuex

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import mitt from 'mitt'
-
-declare global {
-  interface Window {eventBus: any}
-}
-
-window.eventBus = mitt()
 
 import '@/assets/css/tailwind.css'
+import store from './store'
 
 createApp(App)
+  .use(store)
   .use(router)
   .mount('#app')
