a2503d4e7676b7a98425bd42e94f3ecc036ce9b0
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-07 02:46:53 +0100

    Created container for backOfCard

---
+++ src/components/backCardInfoContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="max-w-9/10 sm:max-w-full">
+    <div class="relative cursor-pointer backPage">
+      <img
+        src="@/assets/image/coldCard.jpg"
+        alt="coldCard"
+        class="rounded-lg w-cardImage h-cardImage object-cover "
+      />
+      <div class="absolute inset-0 top-32 text-black">
+        <div
+          class="font-light flex flex-col items-center justify-center text-lg"
+        >
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
