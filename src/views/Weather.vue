0f552f5335d3fd799ef022d23973ced7b6d2a513
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-07 20:19:59 +0100

    Added PROPS to pass data from parent to child

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -3,9 +3,21 @@
     class="bg-gradient-to-t via-red-400 from-yellow-300 to-red-400 min-h-screen"
   >
     <WeatherSearch />
-    <div class="flex justify-center space-x-8">
-      <WeatherCard />
-      <WeatherCard />
+    <div
+      class="flex flex-col md:flex-row justify-center md:space-x-8 items-center"
+    >
+      <WeatherCard
+        v-for="weather in Weather"
+        :key="weather.cityWeather.id"
+        :city="weather.city"
+        :country="weather.countryInfo.country"
+        :status="weather.cityWeather[0].description"
+        :Cloudiness="weather.cloudiness.all"
+        :Temperature="weather.temperature.temp"
+        :SensoryTemp="weather.temperature.feels_like"
+        :windSpeed="weather.wind.speed"
+        :windDeg="weather.wind.deg"
+      />
     </div>
   </div>
 </template>
@@ -14,11 +26,20 @@
 import { defineComponent } from 'vue'
 import WeatherSearch from '@/components/weatherSearch.vue'
 import WeatherCard from '@/components/weatherCard.vue'
+import { useStore } from 'vuex'
 
 export default defineComponent({
   components: { WeatherSearch, WeatherCard },
   setup() {
-    return {}
+    const store = useStore()
+
+    const Weather = store.state.WeatherData
+
+    return {
+      Weather
+    }
   }
 })
 </script>
+
+<style lang="css" scoped></style>
