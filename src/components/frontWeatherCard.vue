b5de55905cac681367e513487a9434ba61a81550
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-07 02:21:16 +0100

    Added component for front of the card

---
+++ src/components/frontWeatherCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="max-w-9/10 sm:max-w-full ">
+    <div class="relative cursor-pointer frontPage">
+      <img
+        src="@/assets/image/coldCard.jpg"
+        alt="coldCard"
+        class="rounded-lg w-cardImage h-cardImage object-cover"
+      />
+      <h2
+        class="absolute top-midY left-midX text-black text-center text-4xl font-light"
+      >
+        Michalovce
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
