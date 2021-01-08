36a95454c1099e28d959b34999e672ddf1efb5c6
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-08 15:58:55 +0100

    Removed reactive replaced by REF

--- src/components/WeatherSearch.vue
+++ src/components/WeatherSearch.vue
@@ -1,25 +1,25 @@
 <template>
   <section class="flex flex-col justify-center items-center h-30vh">
-    <div class="flex flex-col items-center max-w-full">
+    <div class="flex flex-col items-center max-w-full justify-center">
       <h1 class="text-3xl leading-loose text-black">WeatherApp</h1>
-      <div class="relative">
+      <div class="relative max-w-full text-center flex flex-col items-center">
         <input
           type="text"
           class="outline-none opacity-80 focus:opacity-90 focus:ring-2 focus:ring-red-500 focus:border-transparent appearance-none border border-transparent max-w-9/10 sm:max-w-full w-112 py-2 px-4 bg-white text-gray-800 placeholder-gray-400 shadow-md rounded-lg text-base capitalize"
-          :class="{ shakingEffect: City.NotExist }"
+          :class="{ shakingEffect: NotExist }"
           spellcheck="false"
           placeholder="City"
           v-model="searchWeather"
           @keyup="search"
         />
         <transition name="popUpCard">
-          <section
-            class="bg-white opacity-75 mt-1 rounded-lg p-3 cursor-pointer hover:opacity-80 absolute w-full"
+          <button
+            class="outline-none focus:outline-none bg-white opacity-75 hover:opacity-80 mt-1 p-3 text-left capitalize rounded-lg cursor-pointer absolute top-12 w-112 max-w-9/10 sm:max-w-full"
             v-if="searchWeather"
             @click="sendData"
           >
             {{ searchWeather }}
-          </section>
+          </button>
         </transition>
       </div>
     </div>
@@ -27,7 +27,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, reactive } from 'vue'
+import { defineComponent, ref } from 'vue'
 import axios from 'axios'
 import { debounce } from 'lodash-es'
 import { WeatherInfo } from '@/types/vueInterface.ts'
@@ -36,14 +36,14 @@ import { useStore } from 'vuex'
 export default defineComponent({
   setup() {
     const store = useStore()
-    const City = reactive({
-      NotExist: false
-    })
+    const KEY = 'f647bd369ac5888e2c0e377ef80fb4f5'
+    let weatherData: any
+
+    const NotExist = ref(false)
     const popUpCard = ref(false)
 
     const removeWhiteSpace = /^\s/
     const searchWeather = ref('')
-    const KEY = 'f647bd369ac5888e2c0e377ef80fb4f5'
 
     const extractData = ({
       id: id,
@@ -78,37 +78,35 @@ export default defineComponent({
             `https://api.openweathermap.org/data/2.5/weather?q=${searchWeather.value}&appid=${KEY}`
           )
           .then(res => {
-            const weatherData = res.data
+            weatherData = res.data
             extractData(weatherData)
-            store.commit('PUSHDATA', extractData(weatherData))
-            setTimeout(() => {
-              searchWeather.value = ''
-            }, 100)
+            console.log(weatherData)
           })
           .catch(err => {
             if (err) {
-              City.NotExist = true
+              NotExist.value = true
               searchWeather.value = ''
               setTimeout(() => {
-                City.NotExist = false
+                NotExist.value = false
               }, 1000)
             }
           })
       }
     }
     const search = debounce(() => {
-      fetchWeather()
+      // fetchWeather()
     }, 750)
 
-    const sendData = () => {
-      console.log('TEST')
+    const sendData = async () => {
+      // store.commit('PUSHDATA', extractData(weatherData))
+      console.log(await extractData(weatherData))
     }
 
     return {
       searchWeather,
       fetchWeather,
       search,
-      City,
+      NotExist,
       popUpCard,
       sendData
     }
