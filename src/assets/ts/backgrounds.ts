8f4824135d664bf2b62fe920ceaa43fd7f916f41
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-12 20:02:08 +0100

    Changed: type to any

--- src/assets/ts/backgrounds.ts
+++ src/assets/ts/backgrounds.ts
@@ -1,10 +1,8 @@
-import { backgroundColors } from '@/types/vueInterface'
-
-export const backgrounds: backgroundColors = {
-	Snow: 'via-blue-100 from-blue-100 to-blue-500',
-	Thunderstorm: 'via-gray-600 from-gray-600 to-blue-700',
-	Rain: 'via-indigo-400 from-indigo-400 to-blue-700',
-	Drizzle: 'via-blue-100 from-blue-100 to-blue-300',
-	Clouds: 'via-blue-200 from-blue-200 to-gray-400',
-	Clear: 'via-blue-500 from-blue-500 to-yellow-400'
+export const backgrounds: any = {
+  Snow: ['via-blue-100', 'from-blue-100', 'to-blue-500'],
+  Thunderstorm: ['via-gray-600', 'from-gray-600', 'to-blue-700'],
+  Rain: ['via-indigo-400', 'from-indigo-400', 'to-blue-700'],
+  Drizzle: ['via-blue-100', 'from-blue-100', 'to-blue-300'],
+  Clouds: ['via-blue-200', 'from-blue-200', 'to-gray-400'],
+  Clear: ['via-green-500', 'from-blugreen-500', 'to-yellow-400']
 }
