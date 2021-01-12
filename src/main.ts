cc81d2ffcb5f003228157c25be1c79c60ec30df6
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-12 13:26:06 +0100

    Removed: Vuex

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import '@/assets/css/tailwind.css'
-import store from './store'
 
 createApp(App)
-  .use(store)
   .use(router)
   .mount('#app')
