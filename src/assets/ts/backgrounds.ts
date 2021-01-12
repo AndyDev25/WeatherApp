258c2782885e04a67b5af5bf334b38a3089797a7
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-01-12 18:00:52 +0100

    Added: background colors for weather

---
+++ src/assets/ts/backgrounds.ts
@@ -0,0 +1,10 @@
+import { backgroundColors } from '@/types/vueInterface'
+
+export const backgrounds: backgroundColors = {
+	Snow: 'via-blue-100 from-blue-100 to-blue-500',
+	Thunderstorm: 'via-gray-600 from-gray-600 to-blue-700',
+	Rain: 'via-indigo-400 from-indigo-400 to-blue-700',
+	Drizzle: 'via-blue-100 from-blue-100 to-blue-300',
+	Clouds: 'via-blue-200 from-blue-200 to-gray-400',
+	Clear: 'via-blue-500 from-blue-500 to-yellow-400'
+}
